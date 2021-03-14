0523973003c4f0abf19486417c697ec2793c106f
ashwinigit123 <ashwinitrale8@gmail.com>
2021-03-13 20:47:35 -0600

    login nav

--- signup.js
+++ signup.js
@@ -1,8 +1,26 @@
 function sendMail() {
+		alert('in sendemail');
 			var emailID=document.getElementById("email").value;
 			var password=document.getElementById('pwd').value;
 			var subject = ('Welcome to YouareonDefault');
 			var body = ('Here are your credentials- %0DLogin email:' +emailID+ '%0DPassword :'+password+ '%0D%0DThank you') ;
-			document.write('<a href="mailto:' + emailID + '?subject=' +subject+ '&body=' +body+ '">' + 'Click here to get an email with credentials' + '<'+'/a>');
-			location.href = "Login.html";
+			//document.write('<a href="mailto:' + emailID + '?subject=' +subject+ '&body=' +body+ '">' + 'Click here to get an email with credentials' + '<'+'/a>');
+			//location.href = "Login.html";
+			var link = "mailto:"+emailID
+             + "?subject=" + encodeURIComponent(subject)
+             + "&body=" + encodeURIComponent(body)
+    		 ;
+
+    	window.location.href = link;
+				alert('You have registered successfully and Email has been sent. Click on login to go to login page.');
+		}
+
+		function redirectUser(){
+			var emailID = document.getElementById("em").value;
+			alert('email:'+emailID);
+			if(emailID.includes('abc'))
+			{
+				alert('true')
+				location.href="https://www.w3schools.com/howto/howto_css_login_form.asp"
+			}
 		}
